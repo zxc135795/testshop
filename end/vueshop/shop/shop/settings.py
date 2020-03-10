5e802d15f91e9ffe5002026d471fd51bbc208a34
zxc135795 <2388446799@qq.com>
2020-03-10 18:55:34 +0800

    后端 初测

--- end/vueshop/shop/shop/settings.py
+++ end/vueshop/shop/shop/settings.py
@@ -39,9 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myshop',
     'rest_framework',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
